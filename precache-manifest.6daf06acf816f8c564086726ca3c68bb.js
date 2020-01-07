d87ab839b7abe8c8642d53d34832bb71b62d4d26
useryansong <useryansong@gmail.com>
2020-01-06 21:37:07 -0500

    Updates

---
+++ precache-manifest.6daf06acf816f8c564086726ca3c68bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3ae17c575941ac5b40690d34304198",
+    "url": "/React-comment-redux/index.html"
+  },
+  {
+    "revision": "880934ed8ece8cb4f94b",
+    "url": "/React-comment-redux/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a32b791b2a421f14c1ba",
+    "url": "/React-comment-redux/static/css/main.be416e61.chunk.css"
+  },
+  {
+    "revision": "880934ed8ece8cb4f94b",
+    "url": "/React-comment-redux/static/js/2.a0e8ebb9.chunk.js"
+  },
+  {
+    "revision": "a32b791b2a421f14c1ba",
+    "url": "/React-comment-redux/static/js/main.d9e1d2a9.chunk.js"
+  },
+  {
+    "revision": "bdc87661c39714d3c383",
+    "url": "/React-comment-redux/static/js/runtime~main.3336f12b.js"
+  }
+]);
